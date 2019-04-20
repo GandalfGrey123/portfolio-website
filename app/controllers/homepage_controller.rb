dbe852c04867650072949840980546171ee80aed
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-20 03:28:07 -0700

    add homepage controller return two arrays on index render

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,6 @@
 class HomepageController < ApplicationController
   def index
-  	
+  	@current_projects = Project.where(completed: false)
+  	@completed_projects = Project.where(completed: true)
   end
 end
