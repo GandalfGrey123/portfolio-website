602938a8a4e049a7662a8ef468c7a7f833bc69f7
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-05-24 15:18:28 -0700

    create post handler and route for emails

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,4 +3,15 @@ class HomepageController < ApplicationController
   	@current_projects = Project.where(completed: false)
   	@completed_projects = Project.where(completed: true)
   end
+
+  def send_email
+  	new_Message = Message.create!(
+  	 :senderIp => params[:clientIp].to_s,
+  	 :senderEmail => params[:email],
+	 :first => params[:firstName],
+	 :last => params[:lastName],
+	 :title => params[:subjectTitle],
+	 :message => params[:emailBody],
+  	)  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'homepage/index'
-   
+  post 'homepage/index' => "homepage#send_email", as: :send_email
   root 'homepage#index'
 end
