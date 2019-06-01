62d44aafb94f5e2487d29dbed300e4f39e269bf1
Marcus <gandalfgrey@gmx.com>
2019-05-31 20:47:00 -0700

    added about page and link

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'homepage/index'  
   post 'homepage/index' => "homepage#send_email", as: :send_email
   get 'homepage/sent'
+  get 'homepage/about'
   root 'homepage#index'
 end
